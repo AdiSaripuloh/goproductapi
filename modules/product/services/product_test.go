9a02ed883f78eff5e3271aec6373a5a811295e52
Adi Saripuloh <adisaripuloh@gmail.com>
2020-02-21 11:13:20 +0700

    Add unit testing for service

---
+++ modules/product/services/product_test.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"github.com/AdiSaripuloh/goproductapi/modules/product/models"
+	"testing"
+	"time"
+
+	mock_repositories "github.com/AdiSaripuloh/goproductapi/test/mocks/product"
+	"github.com/golang/mock/gomock"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProductService_GetAllProduct(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := mock_repositories.NewMockProductRepository(ctrl)
+
+	mock.EXPECT().GetAll().Return([]models.Product{
+		{ID: uuid.UUID{uuid.V4}, Name: "Product 1", CreatedAt: time.Now()},
+		{ID: uuid.UUID{uuid.V4}, Name: "Product 2", CreatedAt: time.Now()},
+		{ID: uuid.UUID{uuid.V4}, Name: "Product 3", CreatedAt: time.Now()},
+		{ID: uuid.UUID{uuid.V4}, Name: "Product 4", CreatedAt: time.Now()},
+	}, nil)
+
+	service := productService{
+		repository: mock,
+	}
+
+	products, _ := service.GetAll()
+
+	assert.Equal(t, 4, len(products))
+	assert.Equal(t, "Product 1", products[0].Name)
+	assert.Equal(t, "Product 2", products[1].Name)
+	assert.Equal(t, "Product 3", products[2].Name)
+}
